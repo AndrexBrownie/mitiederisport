cd8bd8adf4bb7d8a6511adfa1b9f5c58df854276
AndrexBrownie <ronal.estradam@gmial.com>
2022-07-28 00:58:28 -0500

    Creacion de HostVirtual y controlador por defecto - OK

---
+++ libs/core.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Controllers\HomeController;
+
+class Core {
+
+    public function __construct() {
+
+         $url = isset($_GET['url'])  ? $_GET['url'] : null;
+         $url = rtrim($url, "/");
+         $url = explode('/', $url);
+
+
+        if (empty($url[0])) {
+
+            require_once '../app/controllers/homeController.php';
+            $controller = new HomeController();
+            $controller->index();
+            return false;
+        }
+
+
+        //echo "<pre>", print_r($url), "</pre>";
+        myEchoPre($url);
+        myEchoDump($url);
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+//Arquitectura front controller
+
+require_once "../libs/core.php";
+require_once "../app/helpers/helpers.php";
+
+$Core = new Core();
